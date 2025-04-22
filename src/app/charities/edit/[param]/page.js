3ffcf36e0538977b6c6ffdb1655788ec6b867c9b
Ben Johnson <greypilgrim115@gmail.com>
2025-04-21 21:45:00 -0500

    user specific charities now functional

--- src/app/charities/edit/[param]/page.js
+++ src/app/charities/edit/[param]/page.js
@@ -1,23 +1,38 @@
 'use client';
 
-// import React, { useEffect, useState } from 'react';
-// import PropTypes from 'prop-types';
-// import CharityForm from '../../../../components/CharityForm';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import CharityForm from '../../../../components/CharityForm';
+import { getSingleCharity } from '../../../../api/charityAPI';
 
-// export default function EditCharity({ params }) {
-//   const [editItem, setEditItem] = useState({});
-//   // TODO: grab the firebasekey
-//   const { param } = params;
+export default function EditCharity({ params }) {
+  const [editItem, setEditItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-//   // TODO: make a call to the API to get the book data
-//   useEffect(() => {
-//     getSingleCharity(param).then(setEditItem);
-//   }, [param]);
+  const { param } = params;
 
-//   // TODO: pass object to form
-//   return <CharityForm obj={editItem} />;
-// }
+  useEffect(() => {
+    if (param) {
+      getSingleCharity(param)
+        .then((data) => {
+          setEditItem(data);
+          setIsLoading(false);
+        })
+        .catch((er) => {
+          setError('Failed to load author data.');
+          setIsLoading(false);
+          console.log(er);
+        });
+    }
+  }, [param]);
 
-// EditCharity.propTypes = {
-//   params: PropTypes.objectOf({}).isRequired,
-// };
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return <CharityForm obj={editItem} />;
+}
+
+EditCharity.propTypes = {
+  params: PropTypes.objectOf({}).isRequired,
+};
