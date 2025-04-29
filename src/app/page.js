dba66e56b67f96016a732060c9cc1c01fcc53eb5
Ben Johnson <greypilgrim115@gmail.com>
2025-04-28 21:12:22 -0500

    removed 'all events' and 'home' from navbar. Added short welcome message when icon is clicked

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,64 @@
 'use client';
 
-// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
 import React, { useEffect, useState } from 'react';
-// import { useAuth } from '@/utils/context/authContext';
-import { myCharities } from '../api/charityAPI';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+
+// import Link from 'next/link';
+import { useAuth } from '../utils/context/authContext';
 import CharityCard from '../components/CharityCard';
+import { getCharities } from '../api/charityAPI';
 
 function Home() {
   // TODO: Set a state for books
   const [charities, setCharities] = useState([]);
 
   // TODO: Get user ID using useAuth Hook
-  // const { user } = useAuth();
+  const { user } = useAuth();
+
+  const getAllCharities = () => {
+    getCharities().then(setCharities);
+  };
 
   useEffect(() => {
-    myCharities().then((data) => {
-      const charitiesArray = Array.isArray(data) ? data : [data];
-      setCharities(charitiesArray);
-    });
+    getAllCharities();
+    console.log(user);
   }, []);
 
   return (
-    // <div
-    //   className="text-center d-flex flex-column justify-content-center align-items-center"
-    //   style={{
-    //     height: '90vh',
-    //     padding: '30px',
-    //     maxWidth: 'auto',
-    //     margin: '0 auto',
-    //     background: '#D3F2E3',
-    //   }}
-    // >
-    <div className="text-center my-4">
-      <div className="d-flex flex-wrap">
-        {charities.map((charity) => {
-          console.log('Rendering charity:', charity);
-          return <CharityCard key={charity.id} charityObj={charity} />;
-        })}
+    <div
+      className="text-center d-flex flex-column justify-content-center align-items-center"
+      style={{
+        height: '90vh',
+        padding: '30px',
+        maxWidth: 'auto',
+        marginTop: '100px',
+        margin: '0 auto',
+        background: '#D3F2E3',
+      }}
+    >
+      <div
+        className="text-center my-4"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: 'auto',
+          marginTop: '100px',
+          margin: '0 auto',
+          background: '#D3F2E3',
+        }}
+      >
+        <h1>Find your favorite charities on GiveHub!</h1>
+        <div className="addCharityBtn">
+          <Link href="/charities/new" passHref>
+            <Button>Add A Charity</Button>
+          </Link>
+        </div>
+        <div className="d-flex flex-wrap">
+          {charities.map((charity) => (
+            <CharityCard key={charity.id} charityObj={charity} onUpdate={getAllCharities} />
+          ))}
+        </div>
       </div>
     </div>
   );
