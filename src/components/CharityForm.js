5fde25f621a0dba9e181b7a622aeab32031e7bdc
Ben Johnson <greypilgrim115@gmail.com>
2025-04-29 20:48:25 -0500

    added tags to charity cards

--- src/components/CharityForm.js
+++ src/components/CharityForm.js
@@ -60,58 +60,92 @@ function CharityForm({ obj = initialState }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit} className="text-black">
-      <h2 className="text-white mt-5">{obj.id ? 'Update' : 'Create'} Charity</h2>
+    <div className="formDiv" style={{ width: '50%' }}>
+      <Form onSubmit={handleSubmit} className="text-black">
+        <h2 className="text-white mt-5">{obj.id ? 'Update' : 'Create'} Charity</h2>
 
-      <FloatingLabel controlId="floatingInput1" label="Charity Name" className="mb-3">
-        <Form.Control type="text" placeholder="Charity Name" name="name" value={formInput.name} onChange={handleChange} required />
-      </FloatingLabel>
+        <FloatingLabel controlId="floatingInput1" label="Charity Name" className="mb-3">
+          <Form.Control type="text" placeholder="Charity Name" name="name" value={formInput.name} onChange={handleChange} required />
+        </FloatingLabel>
 
-      <FloatingLabel controlId="floatingInput2" label="Image" className="mb-3">
-        <Form.Control type="url" placeholder="Enter an image URL" name="image" value={formInput.image} onChange={handleChange} required />
-      </FloatingLabel>
+        <FloatingLabel controlId="floatingInput2" label="Image" className="mb-3">
+          <Form.Control type="url" placeholder="Enter an image URL" name="image" value={formInput.image} onChange={handleChange} required />
+        </FloatingLabel>
 
-      <FloatingLabel controlId="floatingInput3" label="Description" className="mb-3">
-        <Form.Control type="text" placeholder="Description" name="description" value={formInput.description} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingInput1" label="Street" className="mb-3">
-        <Form.Control type="text" placeholder="Street" name="street" value={formInput.street} onChange={handleChange} required />
-      </FloatingLabel>
+        <FloatingLabel controlId="floatingInput3" label="Description" className="mb-3">
+          <Form.Control type="text" placeholder="Description" name="description" value={formInput.description} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput1" label="Street" className="mb-3">
+          <Form.Control type="text" placeholder="Street" name="street" value={formInput.street} onChange={handleChange} required />
+        </FloatingLabel>
 
-      <FloatingLabel controlId="floatingTextarea" label="City" className="mb-3">
-        <Form.Control type="text" placeholder="City" style={{ height: '100px' }} name="city" value={formInput.city} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="State" className="mb-3">
-        <Form.Control type="text" placeholder="State" style={{ height: '100px' }} name="state" value={formInput.state} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="Zip" className="mb-3">
-        <Form.Control type="text" placeholder="Zip Code" style={{ height: '100px' }} name="zip" value={formInput.zip} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="contactName" className="mb-3">
-        <Form.Control type="text" placeholder="Contact Name" style={{ height: '100px' }} name="contactName" value={formInput.contactName} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="Email" className="mb-3">
-        <Form.Control type="text" placeholder="Email" style={{ height: '100px' }} name="contactEmail" value={formInput.contactEmail} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="Phone" className="mb-3">
-        <Form.Control type="text" placeholder="Phone" style={{ height: '100px' }} name="contactPhone" value={formInput.contactPhone} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea" label="Website" className="mb-3">
-        <Form.Control type="text" placeholder="State" style={{ height: '100px' }} name="website" value={formInput.website} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingInput2" label="Owners" className="mb-3">
-        <Form.Control type="text" placeholder="Owners" name="owners" value={formInput.owners} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingInput2" label="Donations" className="mb-3">
-        <Form.Control type="text" placeholder="Donations" name="donations" value={formInput.donations} onChange={handleChange} required />
-      </FloatingLabel>
-      {/* <FloatingLabel controlId="floatingTextarea" label="Stars" className="mb-3">
+        <FloatingLabel controlId="floatingTextarea" label="City" className="mb-3">
+          <Form.Control type="text" placeholder="City" style={{ height: '100px' }} name="city" value={formInput.city} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="State" className="mb-3">
+          <Form.Control type="text" placeholder="State" style={{ height: '100px' }} name="state" value={formInput.state} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="Zip" className="mb-3">
+          <Form.Control type="text" placeholder="Zip Code" style={{ height: '100px' }} name="zip" value={formInput.zip} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="contactName" className="mb-3">
+          <Form.Control type="text" placeholder="Contact Name" style={{ height: '100px' }} name="contactName" value={formInput.contactName} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="Email" className="mb-3">
+          <Form.Control type="text" placeholder="Email" style={{ height: '100px' }} name="contactEmail" value={formInput.contactEmail} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="Phone" className="mb-3">
+          <Form.Control type="text" placeholder="Phone" style={{ height: '100px' }} name="contactPhone" value={formInput.contactPhone} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="Website" className="mb-3">
+          <Form.Control type="text" placeholder="State" style={{ height: '100px' }} name="website" value={formInput.website} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput2" label="Owners" className="mb-3">
+          <Form.Control type="text" placeholder="Owners" name="owners" value={formInput.owners} onChange={handleChange} required />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput2" label="Donations" className="mb-3">
+          <Form.Control type="text" placeholder="Donations" name="donations" value={formInput.donations} onChange={handleChange} required />
+        </FloatingLabel>
+        {/* <div className='tags' style={{width: '100%', display: 'flex', flexDirection:}} >
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Housing"
+      />
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Food"
+      />
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Education"
+      />
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Job Placement"
+      />
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Recovery"
+      />
+      <Form.Check // prettier-ignore
+        type="switch"
+        id="custom-switch"
+        label="Legal"
+      />
+      </div> */}
+        {/* <FloatingLabel controlId="floatingTextarea" label="Stars" className="mb-3">
         <Form.Control type="text" placeholder="1-5" style={{ height: '100px' }} name="stars" value={formInput.stars} onChange={handleChange} required />
       </FloatingLabel> */}
 
-      {/* SUBMIT BUTTON  */}
-      <Button type="submit">{obj.id ? 'Update' : 'Create'} Charity</Button>
-    </Form>
+        {/* SUBMIT BUTTON  */}
+        <Button type="submit">{obj.id ? 'Update' : 'Create'} Charity</Button>
+      </Form>
+    </div>
   );
 }
 
